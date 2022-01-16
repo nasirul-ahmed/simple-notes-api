72f1542839f990a03fb633ebea5c494da2a8af19
nasirulahmed369 <76066542+nasirul-ahmed@users.noreply.github.com>
2022-01-16 16:27:14 +0530

    m

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const addnotes = require('./src/routes/add_notes/router.add_notes');
 const bodyParser = require("body-parser");
 const getNotes = require('./src/routes/get_notes/router.get_notes');
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(cors())
 
 
 
